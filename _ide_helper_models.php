3ebf4048bb54455880ff63e6c0043ffa59ed192b
Mathieu Bouchonnet <mathieubouchonnet@gmail.com>
2025-08-24 18:54:11 +0200

    Generate helper for exercise and correction

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -14,6 +14,53 @@
 namespace App\Models{
 /**
  * @property int $id
+ * @property string $correct_sentence
+ * @property string|null $comment
+ * @property int $exercise_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Exercise $exercise
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Correction newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Correction newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Correction query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Correction whereComment($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Correction whereCorrectSentence($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Correction whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Correction whereExerciseId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Correction whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Correction whereUpdatedAt($value)
+ */
+	class Correction extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * @property int $id
+ * @property string $sentence
+ * @property string $translation
+ * @property int $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Correction> $corrections
+ * @property-read int|null $corrections_count
+ * @property-read \App\Models\User $user
+ * @method static \Database\Factories\ExerciseFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Exercise newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Exercise newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Exercise query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Exercise whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Exercise whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Exercise whereSentence($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Exercise whereTranslation($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Exercise whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Exercise whereUserId($value)
+ */
+	class Exercise extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * @property int $id
  * @property string $name
  * @property string $email
  * @property \Illuminate\Support\Carbon|null $email_verified_at
@@ -21,6 +68,8 @@ namespace App\Models{
  * @property string|null $remember_token
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Exercise> $exercises
+ * @property-read int|null $exercises_count
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
  * @property-read int|null $notifications_count
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Word> $words
